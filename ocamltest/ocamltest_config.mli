1b7aa61b608a392685806af98059be92a410f9f0
Samuel Hym <samuel.hym@rustyne.lautre.net>
2023-12-07 18:13:03 +0100

    Expose the directory containing winpthreads in ocamltest

--- ocamltest/ocamltest_config.mli
+++ ocamltest/ocamltest_config.mli
@@ -39,6 +39,9 @@ val cflags : string
 val ccomptype : string
 (** Type of C compiler (msvc, cc, etc.) *)
 
+val winpthreads_srcdir : string
+(** Subdirectory containing the sources of Winpthreads (empty if none) *)
+
 val diff : string
 (** Path to the diff tool *)
 
