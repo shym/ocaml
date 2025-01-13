fc91b08e2e85fccccba474b40cddcaf4c4317b79
Samuel Hym <samuel@tarides.com>
2025-01-13 12:19:19 +0100

    Add a `Config` variable for the need of the GNU non-executable stack note
    Add a `Config.nonexecstack_note` variable to indicate whether the
    `.note.GNU-stack` section should be present to mark the stack explicitly
    as non-executable
    
    Need bootstrap

--- utils/config.fixed.ml
+++ utils/config.fixed.ml
@@ -36,6 +36,7 @@ let bytecomp_c_compiler = ""
 let native_c_compiler = c_compiler
 let native_c_libraries = ""
 let native_ldflags = ""
+let nonexecstack_note = false
 let native_pack_linker = boot_cannot_call "the linker"
 let default_rpath = ""
 let mksharedlibrpath = ""
